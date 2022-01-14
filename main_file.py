b4f57556c9fc5ca1f4282150d13d351131d75730
vamsikrish96 <vamsikrishnachemudupati@yahoo.com>
2022-01-14 14:14:00 -0500

    Added the feature of giving input

--- main_file.py
+++ main_file.py
@@ -2,5 +2,15 @@
 # This is the main file
 
 
+
 def find_factorial(n):
+    if(n < 2):
+        return 1
     return n*find_factorial(n-1)
+
+
+print("Enter the number for which we need factoriaL")
+
+n = input()
+ans = find_factorial(int(n))
+print(ans)
